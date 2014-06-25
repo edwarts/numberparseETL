8b60dc6e546e2f7392ecb4245bf3526581d22b14
bo_ma <bo@adbrain.com>
2014-06-25 04:06:47 +0100

    Integration Test Finished

--- parseNumberETL/parseNumberETL/Form1.cs
+++ parseNumberETL/parseNumberETL/Form1.cs
@@ -12,9 +12,15 @@ namespace parseNumberETL
     public partial class Form1 : Form
     {
         static bool fileIsOpen = false;
+        List<decimal> dataset = new List<decimal>();
+        static iFileIO ifs;
+        static iMath ims;
         public Form1()
         {
+
             InitializeComponent();
+            ifs = new FileIO();
+            ims = new MathFunction();
         }
 
         private void bt_opencsv_Click(object sender, EventArgs e)
@@ -29,6 +35,16 @@ namespace parseNumberETL
                 bt_distribution.Enabled = true;
                 bt_mean.Enabled = true;
                 bt_sd.Enabled = true;
+                if (ifs.CheckExist(filePath))
+                {
+                    dataset = ifs.ReadAllNumber(filePath);
+                    MessageBox.Show("File loaded");
+                }
+                else
+                {
+                    MessageBox.Show("File does not exist");
+                }
+
             }
             else
             {
@@ -38,17 +54,23 @@ namespace parseNumberETL
 
         private void bt_mean_Click(object sender, EventArgs e)
         {
-            MessageBox.Show((fileIsOpen==true?"Yes":"No"));
+            MessageBox.Show("Mean is"+ims.GetMean(dataset));
         }
 
         private void bt_sd_Click(object sender, EventArgs e)
         {
-            MessageBox.Show((fileIsOpen == true ? "Yes" : "No"));
+            MessageBox.Show("SD is "+ ims.GetStandDeviation(dataset));
         }
 
         private void bt_distribution_Click(object sender, EventArgs e)
         {
-            MessageBox.Show((fileIsOpen == true ? "Yes" : "No"));
+            StringBuilder outputDistribution = new StringBuilder();
+            List<int> distributions = ims.GetDistributionOfBinTen(dataset);
+            for (int i = 1,k=0; i < distributions.ToArray().Length; i++,k++)
+            {
+                outputDistribution.Append("The number between " + k * 10 + " and " + (k + 1) * 10 + " is " + distributions.ToArray()[i]+"\n");
+            }
+                MessageBox.Show(outputDistribution.ToString());
         }
     }
 }
